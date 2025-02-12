0c3ea8eeba913d11a5f0ddea976ab026afd7a33a
lonewanderer27 <adrianejames27@gmail.com>
2025-02-12 14:28:20 +0800

    chore: move gradient_text to a new file

--- second_flutter_app/lib/main.dart
+++ second_flutter_app/lib/main.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:second_flutter_app/gradient_text.dart';
 
 void main() {
   // using const allows the widgets by the app
   // to be cached, improving performance.
   runApp(MyApp());
-}
-
-class GradientText extends StatelessWidget {
-  const GradientText(
-    this.text, {
-    super.key,
-    required this.gradient,
-    this.style,
-  });
-
-  final String text;
-  final TextStyle? style;
-  final Gradient gradient;
-
-  @override
-  Widget build(BuildContext context) {
-    return ShaderMask(
-      blendMode: BlendMode.srcIn,
-      shaderCallback: (Rect bounds) => gradient.createShader(
-        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-      ),
-      child: Text(text, style: style),
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -72,58 +49,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// // With Flutter, you create user interfaces by combining "widgets"
-// // You'll learn all about them (and much more) throughout this course!
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // Every custom widget must have a build() method
-//   // It tells Flutter, which widgets make up your custom widget
-//   // Again: You'll learn all about that throughout the course!
-//   @override
-//   Widget build(BuildContext context) {
-//     // Below, a bunch of built-in widgets are used (provided by Flutter)
-//     // They will be explained in the next sections
-//     // In this course, you will, of course, not just use them a lot but
-//     // also learn about many other widgets!
-//     return MaterialApp(
-//       title: 'Flutter First App',
-//       theme: ThemeData(useMaterial3: true),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Welcome to Flutter'),
-//         ),
-//         body: Container(
-//           width: double.infinity,
-//           padding: const EdgeInsets.all(12),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: const [
-//               Text(
-//                 'Flutter - The Complete Guide',
-//                 textAlign: TextAlign.center,
-//                 style: TextStyle(
-//                   fontSize: 24,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               SizedBox(height: 16),
-//               Text(
-//                 'Learn Flutter step-by-step, from the ground up.',
-//                 textAlign: TextAlign.center,
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
