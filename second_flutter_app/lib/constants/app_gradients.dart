80bfcb1beac1f71499bccf889701425b8408d2d1
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-12 14:52:04 +0800

    chore: move stratpointGradient to proper AppGradients class

---
+++ second_flutter_app/lib/constants/app_gradients.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  static const LinearGradient stratpointGradient =
+      LinearGradient(colors: <Color>[Color(0xFF00bbf8), Color(0xFF007ad6)]);
+}
--- second_flutter_app/lib/main.dart
+++ second_flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_flutter_app/constants/app_gradients.dart';
 import 'package:second_flutter_app/gradient_text.dart';
 
 void main() {
@@ -10,42 +11,43 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final String description = 'Fast forward to the future';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(color: Colors.black),
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Wrap(
-                children: [
-                  GradientText('strat',
-                      gradient: LinearGradient(colors: <Color>[
-                        Color(0xFF00bbf8),
-                        Color(0xFF007ad6)
-                      ]),
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
-                  Text(
-                    'point',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              GradientText(
-                'Fast forward to the future',
-                gradient: LinearGradient(
-                    colors: <Color>[Color(0xFF00bbf8), Color(0xFF007ad6)]),
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-              )
-            ],
-          )),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Wrap(
+                  children: [
+                    GradientText('strat',
+                        gradient: AppGradients.stratpointGradient,
+                        style: TextStyle(
+                            fontSize: 50, fontWeight: FontWeight.bold)),
+                    Text(
+                      'point',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                GradientText(
+                  description,
+                  gradient: AppGradients.stratpointGradient,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                )
+              ],
+            )),
+          ),
         ),
       ),
     );
