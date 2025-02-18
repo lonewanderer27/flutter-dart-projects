9dc96171029b7eb9f56ba80d987136a189f40247
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-18 10:50:12 +0800

    style: align expense title to left

--- expense_tracker/lib/widgets/expense_item.dart
+++ expense_tracker/lib/widgets/expense_item.dart
@@ -13,12 +13,15 @@ class ExpenseItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title, style: TextStyle(fontWeight: FontWeight.bold)),
             SizedBox(height: 4),
             Row(
               children: [
-                Text('₱ ${expense.amount.toStringAsFixed(2)}'),
+                Text(
+                  '₱ ${expense.amount.toStringAsFixed(2)}',
+                ),
                 const Spacer(),
                 Row(
                   children: [
