643314cf0dce4d7f1d8b185c0b0d19131b92efa0
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-18 15:31:35 +0800

    feat: implement expense modal with input validation and date picker

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:expense_tracker/data/expenses_data.dart';
+import 'package:expense_tracker/modals/expenses_modal.dart';
 import 'package:expense_tracker/widgets/expenses_chart.dart';
 import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ExpenseTrackerApp());
+  runApp(MaterialApp(home: ExpenseTrackerApp()));
 }
 
 class ExpenseTrackerApp extends StatefulWidget {
@@ -15,23 +16,37 @@ class ExpenseTrackerApp extends StatefulWidget {
 }
 
 class _ExpenseTrackerAppState extends State<ExpenseTrackerApp> {
+  void _openAddExpenseModal() {
+    // Flutter automatically adds a global context variable
+    // that's why we can access this even though it's below.
+    // We rename builder context as ctx as to not clash
+    // with the global context variable.
+    showModalBottomSheet(
+        context: context, builder: (ctx) => ExpensesModal());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-            child: Column(
-              children: [
-                ExpensesChart(expenses: expensesData),
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(child: ExpensesList(expenses: expensesData))
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expenses Tracker'),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseModal, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+          child: Column(
+            children: [
+              ExpensesChart(expenses: expensesData),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(child: ExpensesList(expenses: expensesData))
+            ],
           ),
         ),
       ),
--- expense_tracker/lib/modals/expenses_modal.dart
+++ expense_tracker/lib/modals/expenses_modal.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/enums/category.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ExpensesModal extends StatefulWidget {
   const ExpensesModal({super.key});
@@ -8,8 +10,145 @@ class ExpensesModal extends StatefulWidget {
 }
 
 class _ExpensesModalState extends State<ExpensesModal> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  String? _date;
+  Category? _category = Category.leisure;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _close() {
+    Navigator.pop(context);
+  }
+
+  void _showDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    if (pickedDate == null) return;
+
+    // set the picked date
+    setState(() {
+      _date = DateFormat('MMMM d, y').format(pickedDate);
+    });
+  }
+
+  void _submitExpense() {
+    final enteredAmt = double.tryParse(_amountController.text);
+
+    // check entered amount
+    final amountInvalid = enteredAmt == null || enteredAmt <= 0;
+
+    // check title
+    final titleInvalid = _titleController.text.trim().isEmpty;
+
+    // check date
+    final dateInvalid = _date == null;
+
+    if (amountInvalid || titleInvalid || dateInvalid) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Incomplete'),
+                content: Text(
+                    'Please make sure a valid ${amountInvalid ? 'amount, ' : ''}${titleInvalid ? 'title, ' : ''}${dateInvalid ? 'date, ' : ''} was entered.'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: Text('OK'))
+                ],
+              ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+                controller: _titleController,
+                maxLength: 50,
+                decoration: InputDecoration(labelText: 'Title')),
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration:
+                      InputDecoration(labelText: 'Amount', prefixText: '₱'),
+                )),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(_date ??= 'Select a date'),
+                      IconButton(
+                          onPressed: _showDatePicker,
+                          icon: Icon(Icons.calendar_month))
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Category:', style: TextStyle(fontSize: 15)),
+                SizedBox(width: 30),
+                Expanded(
+                  child: DropdownButton(
+                    items: Category.values
+                        .map((category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name.toUpperCase())))
+                        .toList(),
+                    onChanged: (selected) {
+                      if (selected == null) return;
+                      setState(() {
+                        _category = selected;
+                      });
+                    },
+                    value: _category,
+                  ),
+                ),
+                SizedBox(width: 20)
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                    child:
+                        TextButton(onPressed: _close, child: Text('Cancel'))),
+                SizedBox(width: 10),
+                Expanded(
+                    child: ElevatedButton(
+                        onPressed: _submitExpense, child: Text('Save')))
+              ],
+            ),
+          ],
+        ));
   }
 }
