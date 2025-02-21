b1941d4df177cc6c06bd13b5a57c8a370329d9e9
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-21 10:06:17 +0800

    build!: upgrade AGP gradle version

--- meals_coffee_lounge/test/widget_test.dart
+++ meals_coffee_lounge/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:meals_coffee_lounge/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MealsByCFLApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
