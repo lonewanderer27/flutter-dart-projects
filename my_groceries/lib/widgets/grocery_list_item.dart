613cf97a7872d14af3373aac0d810a862d410c6c
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-25 22:22:39 +0800

    feat: delete a grocery

--- my_groceries/lib/widgets/grocery_list_item.dart
+++ my_groceries/lib/widgets/grocery_list_item.dart
@@ -1,33 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_groceries/models/grocery_item.dart';
+import 'package:my_groceries/providers/grocery_items_provider.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class GroceryListItem extends StatelessWidget {
+class GroceryListItem extends ConsumerWidget {
   const GroceryListItem({super.key, required this.item});
   final GroceryItem item;
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Skeleton.replace(
-          width: 25,
-          height: 25,
-          child: SizedBox(
-            width: 25.0,
-            height: 25.0,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: item.category.color,
-                  borderRadius: BorderRadius.circular(5)),
-            ),
-          )),
-      title: Skeleton.replace(
-        height: 20,
-        width: 50,
-        child: Text(item.name),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Dismissible(
+      key: Key(item.id),
+      background: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.error,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Delete',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Delete',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
       ),
-      trailing: Skeleton.replace(
-          height: 10, width: 10, child: Text(item.quantity.toString())),
+      onDismissed: (direction) {
+        ref.read(groceryItemsProvider.notifier).deleteItem(item.id);
+      },
+      child: ListTile(
+        leading: Skeleton.replace(
+            width: 25,
+            height: 25,
+            child: SizedBox(
+              width: 25.0,
+              height: 25.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: item.category.color,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            )),
+        title: Skeleton.replace(
+          height: 20,
+          width: 50,
+          child: Text(item.name),
+        ),
+        trailing: Skeleton.replace(
+            height: 10, width: 10, child: Text(item.quantity.toString())),
+      ),
     );
   }
 }
