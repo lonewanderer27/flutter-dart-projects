a503d22b2b1398989d9fe48d8173176a5c1722da
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-02-28 14:44:49 +0800

    style(new_place_screen): update layout to use a stack for image preview and form input

--- moments/lib/screens/new_place_screen.dart
+++ moments/lib/screens/new_place_screen.dart
@@ -27,46 +27,115 @@ class _NewPlaceScreenState extends ConsumerState<NewPlaceScreen> {
         key: _formKey,
         child: Scaffold(
           backgroundColor: Colors.black54,
-          body: SingleChildScrollView(
-            child: Padding(
-              padding:
-                  EdgeInsets.only(top: 40, bottom: 20, right: 20, left: 20),
-              child: Column(
+          body: Stack(
+            children: [
+              Hero(
+                  tag: 'image-preview',
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    clipBehavior: Clip.hardEdge,
+                    child: Image.file(
+                      widget.image,
+                      height: double.infinity,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  )),
+              Expanded(
+                  child: Column(
                 children: [
-                  Hero(
-                      tag: 'image-preview',
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        clipBehavior: Clip.hardEdge,
-                        child: Image.file(widget.image,
-                            height: 500,
-                            width: double.infinity,
-                            fit: BoxFit.fill),
-                      )),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(label: Text('Place')),
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length > 50) {
-                        return 'Please enter a valid place name';
-                      }
-
-                      if (value.trim().length < 2 || value.length > 50) {
-                        return 'Must be between 2 to 50 characters.';
-                      }
-
-                      return null;
-                    },
+                  Expanded(
+                    child: SizedBox.expand(),
                   ),
-                  SizedBox(
-                    height: 20,
+                  Expanded(
+                    flex: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextFormField(
+                              style: TextStyle(color: Colors.white),
+                              controller: _nameController,
+                              decoration: InputDecoration(
+                                  label: Text(
+                                'Caption',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                              validator: (value) {
+                                if (value == null ||
+                                    value.isEmpty ||
+                                    value.length > 50) {
+                                  return 'Please enter a valid place name';
+                                }
+
+                                if (value.trim().length < 2 ||
+                                    value.length > 50) {
+                                  return 'Must be between 2 to 50 characters.';
+                                }
+
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ElevatedButton(
+                                onPressed: handleAdd, child: Text('Submit'))
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                  ElevatedButton(onPressed: handleAdd, child: Text('Submit'))
                 ],
-              ),
-            ),
+              ))
+            ],
           ),
+          // body: SingleChildScrollView(
+          //   child: Padding(
+          //     padding:
+          //         EdgeInsets.only(top: 40, bottom: 20, right: 20, left: 20),
+          //     child: Column(
+          //       children: [
+          //         Hero(
+          //             tag: 'image-preview',
+          //             child: ClipRRect(
+          //               borderRadius: BorderRadius.circular(20),
+          //               clipBehavior: Clip.hardEdge,
+          //               child: Image.file(widget.image,
+          //                   height: 500,
+          //                   width: double.infinity,
+          //                   fit: BoxFit.fill),
+          //             )),
+          //         SizedBox(height: 10),
+          //         TextFormField(
+          //           controller: _nameController,
+          //           decoration: InputDecoration(label: Text('Place')),
+          //           validator: (value) {
+          //             if (value == null || value.isEmpty || value.length > 50) {
+          //               return 'Please enter a valid place name';
+          //             }
+
+          //             if (value.trim().length < 2 || value.length > 50) {
+          //               return 'Must be between 2 to 50 characters.';
+          //             }
+
+          //             return null;
+          //           },
+          //         ),
+          //         SizedBox(
+          //           height: 20,
+          //         ),
+          //         ElevatedButton(onPressed: handleAdd, child: Text('Submit'))
+          //       ],
+          //     ),
+          //   ),
+          // ),
         ));
   }
 }
