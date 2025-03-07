882fecf93c59cb4e49e632fe5c0c3a4c727779c5
i_adrianejames.puzon <i_adrianejames.puzon@stratpoint.com>
2025-03-07 17:25:45 +0800

    fix: ThreadsScreen typo in main.dart

--- warp_chats/lib/main.dart
+++ warp_chats/lib/main.dart
@@ -35,7 +35,7 @@ class App extends StatelessWidget {
               }
 
               if (snapshot.hasData) {
-                return const Threads_Screen();
+                return const ThreadsScreen();
               }
               return const SigninScreen();
             }));
